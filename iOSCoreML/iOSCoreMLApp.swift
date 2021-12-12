f07f731504fc095a621e6d092dae2e600dfe3274
JiwonHae <hjiwon1994@gmail.com>
2021-12-13 01:26:14 +0900

    Initial Commit

---
+++ iOSCoreML/iOSCoreMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSCoreMLApp.swift
+//  iOSCoreML
+//
+//  Created by Jiwon_Hae on 2021/12/13.
+//
+
+import SwiftUI
+
+@main
+struct iOSCoreMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
